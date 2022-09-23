ca98205421f4d39f50282963c6f35b10d0e0f484
Simran Gangwani <simrangngwani3107@gmail.com>
2022-09-23 06:07:28 +0530

    feat: added responsiveness

--- app.js
+++ app.js
@@ -97,3 +97,28 @@ var x = setInterval(function () {
     document.getElementById("second").innerHTML = "0";
   }
 }, 1000);
+
+// responsive nav
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".menu");
+
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}
+
+var scroll = new SmoothScroll('a[href*="#"]', {
+  speed: 1000,
+  speedAsDuration: true,
+});
